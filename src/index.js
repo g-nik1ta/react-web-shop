82dcfe02fe40806f0787f586d5ed661ea6eb143c
Nikita Gonohov <gonohovn@gmail.com>
2023-04-10 03:11:47 +0300

    fixed bug of route

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store} >
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
